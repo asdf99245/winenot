d4a279d4765f252c7280b96bb3377a375e7f5f83
asdf99245 <asdf99245@naver.com>
2023-01-26 21:57:57 +0900

    feat: Noto sans kr font 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css';
+import GlobalStyles from '@/styles/GlobalStyles';
+import theme from '@/styles/theme';
+import { notoSansKR } from '@/utils/fonts';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div className={notoSansKR.className}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
